8acc6237a03c822ed2fa4d0fac8e6303932fa274
LucasNemeth <l_orion_n@hotmail.com>
2020-04-11 16:20:12 -0400

    refactoring some bugs

---
+++ script.js
@@ -0,0 +1,9 @@
+// noSlide = (maxWidth) => {
+//     if (maxWidth.matches) {
+//         alert("hi");
+//     }
+// }
+
+// const maxWidth = ("(max-width:410px)")
+// noSlide(maxWidth)
+// maxWidth.addListener(noSlide)
