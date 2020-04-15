cdca26aba18cd73bdcafe95554022dac931acd9a
LucasNemeth <l_orion_n@hotmail.com>
2020-04-15 11:49:42 -0400

    fixing up nav

---
+++ script.js
@@ -0,0 +1,29 @@
+$(function () {
+    //----click on hamburger button to drop down the menu----//
+    $(".hamburger").click(function () {
+        // ---toggling class ----//
+        $(".top-nav").toggleClass('active');
+        
+    })
+});
+
+
+// $('.menu-toggle').on('click', function () {
+//     $('.menu').addClass('open');
+// });
+
+// $('.menu li').on("click", function () {
+//     $('.menu').removeClass('open');
+// });
+
+noSlide = (maxWidth) => {
+    if (maxWidth.matches) {
+        ('data-aos') === null;
+        $('.left-container').removeData('data-aos');
+        $('.right-container').removeClass('data-aos');
+    }
+}
+
+const maxWidth = window.matchMedia("(max-width:410px)")
+noSlide(maxWidth)
+maxWidth.addListener(noSlide)
